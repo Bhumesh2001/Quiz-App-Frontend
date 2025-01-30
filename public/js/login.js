0e24de760ce6e2bc9d7e2703c70fcc87f14807f1
bhumesh2001 <bhumesh21@navgurukul.org>
2025-01-30 13:55:09 +0530

    latest coe

--- public/js/login.js
+++ public/js/login.js
@@ -31,6 +31,7 @@ document.getElementById('loginForm').addEventListener('submit', async function (
         const response = await fetch(apiUrl, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
             body: JSON.stringify({ email, password }),
         });
 
